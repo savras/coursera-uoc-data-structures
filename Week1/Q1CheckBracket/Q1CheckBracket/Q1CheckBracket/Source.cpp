9ad8c9b9d6afa42ace50a36644e79d97b46baea6
Vincent Gim <vgim@sateva.com.au>
2017-01-15 23:38:19 +0800

    Fixed compile error and finished implementation

--- Week1/Q1CheckBracket/Q1CheckBracket/Q1CheckBracket/Source.cpp
+++ Week1/Q1CheckBracket/Q1CheckBracket/Q1CheckBracket/Source.cpp
@@ -1,11 +1,52 @@
-#include"Stack.h"
-#include <vector>
+#include <iostream>
+#include <string>
+#include <utility>
+#include "Stack.h"
 
-using std::vector;
+using std::cin;
+using std::cout;
+using std::string;
+using std::pair;
+using std::make_pair;
+
+void validateBrackets(const string &text, Stack<pair<int, char>> stack) {
+	const int offset = 1;
+	char val;
+	for (int i = 0; i < text.length(); i++) {
+		val = text[i];
+		if (val == '[' || val == '{' || val == '(') {
+			stack.push(make_pair(i, text[i]));
+		}
+		else if (val == ']' || val == '}' || val == ')') {
+			if (stack.size() == 0) {
+				cout << i + offset;
+				return;
+			}
+
+			if ((val == ']' && stack.top().second != '[') ||
+				(val == '}' && stack.top().second != '{') ||
+				(val == ')' && stack.top().second != '(')) {
+				cout << i + offset;
+				return;
+			}
+
+			stack.pop();
+		}
+	}
+
+	if (stack.size() != 0) {
+		cout << stack.top().first + offset;
+		return;
+	}
+
+	cout << "Success";
+}
 
 int main() {
-	Stack<char> stack;
-	stack.push('c');
+	Stack<pair<int, char>> stack;
+	string text;
+	cin >> text;
 
+	validateBrackets(text, stack);
 	return 0;
 }
--- Week1/Q1CheckBracket/Q1CheckBracket/Q1CheckBracket/Stack.h
+++ Week1/Q1CheckBracket/Q1CheckBracket/Q1CheckBracket/Stack.h
@@ -1,16 +1,45 @@
 #pragma once
 #include <vector>
+#include <stdexcept>
 
-using std::vector;
+using std::out_of_range;
 
 template <typename T>	// same as <class T>
 class Stack
 {
+	std::vector<T> _elements;
 public:
 	void push(const T&);	// http://stackoverflow.com/questions/2640446/why-do-some-people-prefer-t-const-over-const-t
-	T pop() const;
+	T pop();
 	T top() const;	// Cannot modify _elements unless marked mutable.
-private:
-	vector<T> _elements;
+	int size() const;
 };
 
+// Can't split this: http://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
+template <typename T>
+void Stack<T>::push(const T& elem) {
+	_elements.push_back(elem);
+}
+
+template <typename T>
+T Stack<T>::pop() {
+	if (_elements.empty()) {
+		throw out_of_range("Stack::pop(): stack is empty");
+	}
+
+	_elements.pop_back();
+};
+
+template <typename T>
+T Stack<T>::top() const {
+	if (_elements.empty()) {
+		throw out_of_range("Stack::top(): stack is empty");
+	}
+
+	return _elements.back();
+};
+
+template <typename T>
+int Stack<T>::size() const {
+	return _elements.size();
+}
