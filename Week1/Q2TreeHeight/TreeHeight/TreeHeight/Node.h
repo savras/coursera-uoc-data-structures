5954eeda8f2dc6c2f91ac53ed81d5e6d3d999bb7
Vincent Gim <vgim@sateva.com.au>
2017-01-17 09:13:36 +0800

    Testing out unique_ptr

--- Week1/Q2TreeHeight/TreeHeight/TreeHeight/Node.h
+++ Week1/Q2TreeHeight/TreeHeight/TreeHeight/Node.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <vector>
+#include <memory>
+
+using std::unique_ptr;
 
 template <typename T>
 class Node
 {
 public:
 	T value;
-	std::vector<Node<T>> children;
+	std::vector<unique_ptr<Node<T>>> children;
 };
 
--- Week1/Q2TreeHeight/TreeHeight/TreeHeight/Tree.h
+++ Week1/Q2TreeHeight/TreeHeight/TreeHeight/Tree.h
@@ -1,19 +1,30 @@
+// Pointer notes: http://stackoverflow.com/questions/9765052/how-to-pass-a-constant-pointer-to-a-method-in-a-class
+/*
+Node *ptr;  // Non-constant pointer to non-constant data
+Node *const ptr;  // Constant pointer to non-constant data
+const Node *ptr;  // Non-constant pointer to constant data
+Node const *ptr;  // Same as above
+const Node *const ptr;  // Constant pointer to constant data
+Node const *const ptr;  // Same as above
+*/
 #pragma once
 #include "Node.h"
 #include <vector>
 #include <unordered_set>
 #include <algorithm>
 #include <limits>
+#include <memory>
 
 using std::unordered_set;
 using std::vector;
 using std::max;
+using std::unique_ptr;
 
 class Tree
 {
-	Node<int> root;
+	Node<int> *root;
 	vector<Node<int>> nodes;	// http://stackoverflow.com/questions/2275076/is-stdvector-copying-the-objects-with-a-push-back
-	int GetHeightRecursive(const Node<int> &node, int height) const;
+	int GetHeightRecursive(const Node<int> *node, int height) const;
 public:
 	void Build(const vector<int>&);
 	int HeightIterative() const;
@@ -32,10 +43,11 @@ void Tree::Build(const vector<int>& arr) {
 
 		if (parentIndex == -1)
 		{
-			root = nodes[i];
+			root = &nodes[i];
 		}
 		else {
-			nodes[parentIndex].children.push_back(nodes[i]);
+			std::unique_ptr<Node<int>> ptr(&nodes[i]);
+			nodes[parentIndex].children.push_back(std::move(ptr));
 		}
 	}
 }
@@ -44,15 +56,16 @@ int Tree::HeightRecursive() const {
 	return GetHeightRecursive(root, 1);
 }
 
-int Tree::GetHeightRecursive(const Node<int> &node, int height) const {
-	if (node.children.empty()) {
+int Tree::GetHeightRecursive(const Node<int> *node, int height) const {
+	if (node->children.empty()) {
 		return height;
 	}
 
 	int maxHeight = INT_MIN;
-	for (size_t i = 0; i < node.children.size(); i++) {
-		maxHeight = max(maxHeight, GetHeightRecursive(node.children[i], height) + 1);
-	}
+	//for (size_t i = 0; i < node->children.size(); i++) {
+	//	Node<int> *ptr = node->children.at(i).get();
+	//	maxHeight = max(maxHeight, GetHeightRecursive(ptr, height) + 1);
+	//}
 	return maxHeight;
 }
 
