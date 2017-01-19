48a9cad7c930a9e71682ce34164bf01ca632a570
Vincent Gim <vgim@sateva.com.au>
2017-01-20 01:06:04 +0800

    Implemented solution.

--- Week1/Q3NetworkPacket/Q3NetworkPacket/Q3NetworkPacket/Source.cpp
+++ Week1/Q3NetworkPacket/Q3NetworkPacket/Q3NetworkPacket/Source.cpp
@@ -15,7 +15,38 @@ int main() {
 	cin >> S >> n;
 
 	queue<int> queue;
+	vector<pair<int, int>> inputs;
 	int A, P;
-	
+	for (int i = 0; i < n; i++) {
+		cin >> A >> P;
+		inputs.push_back(pair<int, int>(A, P));
+	}
+
+	int runningSum = 0;
+	for (int i = 0; i < n; i++) {
+		A = inputs[i].first;
+		P = inputs[i].second;
+
+		if (!queue.empty()) {
+			runningSum = queue.back();
+		}
+		while (!queue.empty() && queue.front() <= A) {
+			queue.pop();
+		}
+
+		if (queue.empty()) {
+			cout << A << endl;
+			queue.push(runningSum + P);
+		}
+		else if (queue.size() < S) {
+			cout << queue.back() << endl;
+			queue.push(runningSum + P);
+		}
+		else {
+			cout << -1 << endl;
+		}
+	}
+
+	system("pause");
 	return 0;
 }
